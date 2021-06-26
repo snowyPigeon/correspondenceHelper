64b8c3a2f5aad8bde692c89363c60cd52546ab36
Snowy Pigeon <7503186+snowyPigeon@users.noreply.github.com>
2021-06-26 15:54:38 +0100

    UI selection buttons functional
    Context and style buttons toggle on or off.

--- scripts.js
+++ scripts.js
@@ -1,46 +1,166 @@
+// set up default button colors
+var defaultColor = "#FFCCFF";
+document.getElementById("business").style.backgroundColor = defaultColor;
+document.getElementById("school").style.backgroundColor = defaultColor;
+document.getElementById("friends").style.backgroundColor = defaultColor;
+document.getElementById("formal").style.backgroundColor = defaultColor;
+document.getElementById("social").style.backgroundColor = defaultColor;
+document.getElementById("getRecs").style.backgroundColor = defaultColor;
+
+// selected button colors
+var activeColor = "#CCFFFF";
+
+// button states
+var businessOn = false;
+var schoolOn = false;
+var friendsOn = false;
+var formalOn = false;
+var socialOn = false;
+
 // Get the greetings and endings
-function getRecommendations() {
+function getRecommendations(){
 	// query the relevant tables for greeting, title, firstName, surname and ending
-	// getElementById("recommendations").innerHTML += "<p>You chose</p>"; K to replace
+let chosenContext = getSelections();
 }
 
 // Get the chosen context
-function getContext() {
-	
+function getSelections(){
+	var selections = [];
+	if (businessOn == true){
+		selections.push("business");
+	}
+	if (schoolOn == true){
+		selections.push("school");
+	}
+	if (friendsOn == true){
+		selections.push("friends");
+	}
+	if (formalOn == true){
+		selections.push("formal");
+	}
+	if (socialOn == true){
+		selections.push("social");
+	}
+	console.log("You selected: " + selections);
+	return selections;
 }
 
-// Set the chosen context
-function toggleBusiness() {
-	/* if 	(document.getElementById("business").value = false) { K to replace
-		document.getElementById("business").value = true;
+function toggleBusiness(){
+	var business = document.getElementById("business");
+	if (business.style.backgroundColor !== activeColor){
+		document.getElementById("business").style.backgroundColor = activeColor;
+		document.getElementById("school").style.backgroundColor = defaultColor;
+		document.getElementById("friends").style.backgroundColor = defaultColor;
+		businessOn = true;
+		schoolOn = false;
+		friendsOn = false;
 	}
-	else
-	{
-		document.getElementById("business").value = false;
-	} */
+	else {
+		document.getElementById("business").style.backgroundColor = defaultColor;
+		businessOn = false;
+	}
+}
+
+function toggleSchool(){
+	var school = document.getElementById("school");
+	if (school.style.backgroundColor !== activeColor){
+		document.getElementById("school").style.backgroundColor = activeColor;
+		document.getElementById("business").style.backgroundColor = defaultColor;		
+		document.getElementById("friends").style.backgroundColor = defaultColor;
+		schoolOn = true;
+	    businessOn = false;
+		friendsOn = false;
+	}
+	else {
+		document.getElementById("school").style.backgroundColor = defaultColor;
+		schoolOn = false;
+	}
+}
+
+function toggleFriends(){
+	var friends = document.getElementById("friends");
+	if (friends.style.backgroundColor !== activeColor){
+		document.getElementById("friends").style.backgroundColor = activeColor;
+		document.getElementById("school").style.backgroundColor = defaultColor;
+		document.getElementById("business").style.backgroundColor = defaultColor;
+		friendsOn = true;
+		businessOn = false;
+		schoolOn = false;
+	}
+	else {
+		document.getElementById("friends").style.backgroundColor = defaultColor;
+		friendsOn = false;
+	}
+}
+
+function toggleFormal(){
+	var formal = document.getElementById("formal");
+	if (formal.style.backgroundColor !== activeColor){
+		document.getElementById("formal").style.backgroundColor = activeColor;
+		document.getElementById("social").style.backgroundColor = defaultColor;
+		formalOn = true;
+		socialOn = false;
+	}
+	else {
+		document.getElementById("formal").style.backgroundColor = defaultColor;
+		formalOn = false;
+		
+	}
+	console.log("formal toggled");
+}
+
+function toggleSocial(){
+	var social = document.getElementById("social");
+	if (social.style.backgroundColor !== activeColor){
+		document.getElementById("social").style.backgroundColor = activeColor;
+		document.getElementById("formal").style.backgroundColor = defaultColor;
+		socialOn = true;
+		formalOn = false;
+	}
+	else {
+		document.getElementById("social").style.backgroundColor = defaultColor;
+		socialOn = false;
+	}
+	console.log("social toggled");
 }
 
 // Get the chosen style
-function getStyle() {
+function getStyle(){
+	let styleArray = [formal, social];
 	
 }
 
 // Set the chosen style
-function setStyle() {
+function setStyle(){
 	
 }
 
 // Get the title
-function getTitle() {
-	
+function getTitle(){
+	let titleArray = [miss, mr, mrs, ms, mx, doctor];
 }
 
 // Set the title
-function setTitle() {
+function setTitle(){
 	
 }
 
 // Event listeners
 document.getElementById("business").addEventListener("click", 
 toggleBusiness);
+
+document.getElementById("school").addEventListener("click", 
+toggleSchool);
+
+document.getElementById("friends").addEventListener("click", 
+toggleFriends);
+
+document.getElementById("formal").addEventListener("click", 
+toggleFormal);
+
+document.getElementById("social").addEventListener("click", 
+toggleSocial);
+
+document.getElementById("getRecs").addEventListener("click", 
+getRecommendations);
 
