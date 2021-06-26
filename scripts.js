e6c7960f8d7268886d49e909e0a51220aeee1357
Snowy Pigeon <7503186+snowyPigeon@users.noreply.github.com>
2021-06-27 00:48:34 +0100

    Formal Business text functioning

--- scripts.js
+++ scripts.js
@@ -19,15 +19,83 @@ var socialOn = false;
 
 // Get the greetings and endings
 function getRecommendations(){
+let title = document.getElementById("titles").value;
+let firstName = document.getElementById("fname").value;
+let surname = document.getElementById("sname").value;
 let chosenContext = getSelections();
-// get the json object storing our greetings and endings
-const data = require('correspondence.json');
+var data = {
+"title": ["Miss", "Mr", "Mrs", "Ms", "Mx", "Doctor"],
+"formal": {
+	"greetings": [
+		{"business": ["Dear", "For The Attention Of", "To whom it may concern"]},
+		{"school": ["Dear", "For The Attention Of"]},
+		{"friends": ["Dear", "Dearest"]}
+		],
+	"endings": [
+		{"business": ["Yours Sincerely", "Yours Faithfully"]},
+		{"school": ["Yours Sincerely", "Yours Faithfully"]},
+		{"friends": ["Your most loyal friend", "Your humble and obedient servant", "Your friend"]}
+]},
+"social": {
+	"greetings": [
+		{"business": ["Hello", "Hi", "Good morning", "Good afternoon"]},
+		{"school": ["Hello", "Hi", "Good morning", "Good afternoon"]},
+		{"friends": ["Hello", "Hi", "Hey", "Good morning", "Good afternoon"]}
+		],
+	"endings": [
+		{"business": ["Regards", "Kind regards"]},
+		{"school": ["Regards", "Kind regards"]},
+		{"friends": ["Best wishes", "Take care", "Catch you later"]}
+	]
+}
+};
+
 // change the below to get your greetings and endings
-/* $('#response').html("Greeting: " + data.current.weather[0].description + ", Temp: " +
-						data.current.temp + "C" + ", Temp Feels Like: " + data.current.feels_like + "C" + ", Wind Speed: " +
-						new_speed + " MPH" + ", Dew Point: " + data.current.dew_point + "C"); */
 
+if (chosenContext.includes("formal") && chosenContext.includes("business")){
+	console.log("includes works");
+	let greetings = data.formal.greetings[0].business.length;
+	console.log(greetings);
+	for (var i = 0; i < greetings; i++)
+	{
+		if ((firstName == "") && (surname == "")){
+			if (data.formal.greetings[0].business[i].includes("To whom it may concern")){
+			$(".responses").append('<br><textarea id="' + i + '" rows="2" cols="25" readonly>' + data.formal.greetings[0].business[i] + '</textarea><button onclick="clipboard(' + i + ')">Copy text</button>');
+		}
+		else {
+		
+		}
+		}
+		else {
+			if (data.formal.greetings[0].business[i].includes("To whom it may concern")){
+			
+			}
+			else {
+				$(".responses").append('<br><textarea id="' + i + '" rows="2" cols="25" readonly>' + data.formal.greetings[0].business[i] + " " + title + " " + firstName + " " + surname + '</textarea><button onclick="clipboard(' + i + ')">Copy text</button>');
+			}
+		}
+		
 
+		
+	}
+	let endings = data.formal.endings[0].business.length;
+	console.log(endings);
+	for (var i = 0; i < endings; i++)
+	{
+		var j = greetings + i;
+		console.log("value of j: " + j);
+		$(".responses").append('<br><textarea id="' + j + '" rows="2" cols="25" readonly>' + data.formal.endings[0].business[i] + '</textarea><button onclick="clipboard(' + j + ')">Copy text</button>');
+	}
+}
+}
+
+// From https://www.w3schools.com/howto/tryit.asp?filename=tryhow_js_copy_clipboard
+function clipboard(input) {
+  var copyText = document.getElementById(input);
+  copyText.select();
+  copyText.setSelectionRange(0, 99999)
+  document.execCommand("copy");
+  alert("Copied the text: " + copyText.value);
 }
 
 // Get the chosen context and style
@@ -131,27 +199,6 @@ function toggleSocial(){
 	console.log("social toggled");
 }
 
-// Get the chosen style
-function getStyle(){
-	let styleArray = [formal, social];
-	
-}
-
-// Set the chosen style
-function setStyle(){
-	
-}
-
-// Get the title
-function getTitle(){
-	let titleArray = [miss, mr, mrs, ms, mx, doctor];
-}
-
-// Set the title
-function setTitle(){
-	
-}
-
 // Event listeners
 document.getElementById("business").addEventListener("click", 
 toggleBusiness);
@@ -170,4 +217,5 @@ toggleSocial);
 
 document.getElementById("getRecs").addEventListener("click", 
 getRecommendations);
+
 
